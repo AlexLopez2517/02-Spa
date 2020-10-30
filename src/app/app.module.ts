aaada41358c0d357fa7a67fce3ff40d193f11863
AlexLopez2517 <alex2761086444@gmail.com>
2020-10-29 21:50:18 -0600

    Configurar el navbar y otros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/share/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
